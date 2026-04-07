d871037a941ec41ac6d8924e78acc298b8168c4f
Alex <alexander.westerberg.svedberg@gmail.com>
2026-04-07 17:21:15 +0200

    Initial commit - Docker setup and project structure

---
+++ src/test/java/com/thegamecellar/gameservice/TheGameCellarGameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegamecellar.gameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGameCellarGameServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
